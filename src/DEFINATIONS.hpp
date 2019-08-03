7af66807fd61af08d390e3e7b868b00f234e0e2a
sagar <sagarlama826@gmail.com>
2019-08-03 19:40:11 +0545

    Splash State Working

--- src/DEFINATIONS.hpp
+++ src/DEFINATIONS.hpp
@@ -4,7 +4,7 @@
 #define SCREEN_HEIGHT 768
 #define SPLASH_STATE_SHOW_TIME 5.0
 
-#define SPLASH_SCENE_BACKGROUND_FILEPATH "src/Resources/res/splashBackground.png"
+#define SPLASH_SCENE_BACKGROUND_FILEPATH "src/Resources/res/GameLogo.png"
 #define PLAY_BUTTON_FILEPATH "src/Resources/res/Play Button.png"
 #define GAME_TITLE_FILEPATH "src/Resources/res/Game Title.png"
 #define MAIN_MENU_BACKGROUND_FILEPATH "src/Resources/res/Main Menu Background.png"
--- src/SplashState.cpp
+++ src/SplashState.cpp
@@ -15,6 +15,7 @@ namespace Sagar
 		{
 				_data->assets.LoadTexture("Splash State Background",SPLASH_SCENE_BACKGROUND_FILEPATH);
 				_background.setTexture(this->_data->assets.GetTexture("Splash State Background"));
+				_background.setPosition(sf::Vector2f(200.0f,120.0f));
 		}
 
 		void SplashState::HandleInput()
@@ -40,7 +41,7 @@ namespace Sagar
 
 		void SplashState::Draw(float dt)
 		{
-				_data->window.clear();
+				_data->window.clear(sf::Color(255,255,255));
 				_data->window.draw(_background);
 				_data->window.display();
 		}
