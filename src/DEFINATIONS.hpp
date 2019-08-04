109cbde80c8e5caf26f57b83698f90a5174ce297
sagar <sagarlama826@gmail.com>
2019-08-04 21:42:45 +0545

    Splash State Time increased to 3.5s

--- src/DEFINATIONS.hpp
+++ src/DEFINATIONS.hpp
@@ -2,7 +2,7 @@
 
 #define SCREEN_WIDTH 1024
 #define SCREEN_HEIGHT 768
-#define SPLASH_STATE_SHOW_TIME 0.5
+#define SPLASH_STATE_SHOW_TIME 3.5
 
 #define SPLASH_SCENE_BACKGROUND_FILEPATH "src/Resources/res/GameLogo.png"
 #define PLAY_BUTTON_FILEPATH "src/Resources/res/playButton.png"
